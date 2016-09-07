d71c12e827b9753d45fc41aeb15e7e39705eec43
Alexey Zimarev <alzi@abax.no>
2016-09-07 15:21:29 +0200

    Initial commit

---
+++ src/MassTransit.RedisIntegration/RedisSagaConsumeContext.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Threading.Tasks;
+using MassTransit.Context;
+using MassTransit.Logging;
+using MassTransit.Saga;
+using MassTransit.Util;
+using ServiceStack.Model;
+using ServiceStack.Redis.Generic;
+
+namespace MassTransit.RedisIntegration
+{
+    public class RedisSagaConsumeContext<TSaga, TMessage> :
+        ConsumeContextProxyScope<TMessage>,
+        SagaConsumeContext<TSaga, TMessage>
+        where TMessage : class
+        where TSaga : class, ISaga, IHasGuidId
+    {
+        private static readonly ILog Log = Logger.Get<RedisSagaRepository<TSaga>>();
+        private readonly IRedisTypedClient<TSaga> _sagas;
+
+        public RedisSagaConsumeContext(IRedisTypedClient<TSaga> sagas, ConsumeContext<TMessage> context, TSaga instance)
+            : base(context)
+        {
+            Saga = instance;
+            _sagas = sagas;
+        }
+
+        Guid? MessageContext.CorrelationId => Saga.CorrelationId;
+
+        SagaConsumeContext<TSaga, T> SagaConsumeContext<TSaga>.PopContext<T>()
+        {
+            var context = this as SagaConsumeContext<TSaga, T>;
+            if (context == null)
+                throw new ContextException($"The ConsumeContext<{TypeMetadataCache<TMessage>.ShortName}> could not be cast to {TypeMetadataCache<T>.ShortName}");
+
+            return context;
+        }
+
+        Task SagaConsumeContext<TSaga>.SetCompleted()
+        {
+            _sagas.Delete(Saga);
+            IsCompleted = true;
+            if (Log.IsDebugEnabled)
+                Log.DebugFormat("SAGA:{0}:{1} Removed {2}", TypeMetadataCache<TSaga>.ShortName, TypeMetadataCache<TMessage>.ShortName,
+                    Saga.CorrelationId);
+
+            return TaskUtil.Completed;
+        }
+
+        public TSaga Saga { get; }
+        public bool IsCompleted { get; private set; }
+    }
+}
---
+++ src/MassTransit.RedisIntegration/RedisSagaRepository.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Threading.Tasks;
+using MassTransit.Logging;
+using MassTransit.Pipeline;
+using MassTransit.Saga;
+using MassTransit.Util;
+using ServiceStack.Model;
+using ServiceStack.Redis;
+using ServiceStack.Redis.Generic;
+
+namespace MassTransit.RedisIntegration
+{
+    public class RedisSagaRepository<TSaga> : ISagaRepository<TSaga> where TSaga : class, ISaga, IHasGuidId
+    {
+        private static readonly ILog _log = Logger.Get<RedisSagaRepository<TSaga>>();
+        private readonly IRedisClientsManager _clientsManager;
+
+        public RedisSagaRepository(IRedisClientsManager clientsManager)
+        {
+            _clientsManager = clientsManager;
+        }
+
+        public async Task Send<T>(ConsumeContext<T> context, ISagaPolicy<TSaga, T> policy,
+            IPipe<SagaConsumeContext<TSaga, T>> next) where T : class
+        {
+            if (!context.CorrelationId.HasValue)
+                throw new SagaException("The CorrelationId was not specified", typeof(TSaga), typeof(T));
+
+            var sagaId = context.CorrelationId.Value;
+            using (var redis = _clientsManager.GetClient())
+            {
+                var sagas = redis.As<TSaga>();
+                var inserted = false;
+                TSaga instance;
+
+                if (policy.PreInsertInstance(context, out instance))
+                    inserted = await PreInsertSagaInstance<T>(sagas, instance);
+
+                if (instance == null)
+                    instance = sagas.GetById(sagaId);
+
+                if (instance == null)
+                {
+                    var missingSagaPipe = new MissingPipe<T>(sagas, next);
+                    await policy.Missing(context, missingSagaPipe).ConfigureAwait(false);
+                }
+                else
+                {
+                    if (_log.IsDebugEnabled)
+                    {
+                        _log.DebugFormat("SAGA:{0}:{1} Used {2}", TypeMetadataCache<TSaga>.ShortName,
+                            instance.CorrelationId, TypeMetadataCache<T>.ShortName);
+                    }
+                    var sagaConsumeContext = new RedisSagaConsumeContext<TSaga, T>(sagas, context, instance);
+
+                    await policy.Existing(sagaConsumeContext, next).ConfigureAwait(false);
+
+                    if (inserted && !sagaConsumeContext.IsCompleted)
+                        sagas.Store(instance);
+
+                    if (_log.IsDebugEnabled)
+                        _log.DebugFormat("SAGA (Send): New saga state: {@Saga}", instance);
+                }
+            }
+        }
+
+        public Task SendQuery<T>(SagaQueryConsumeContext<TSaga, T> context, ISagaPolicy<TSaga, T> policy,
+            IPipe<SagaConsumeContext<TSaga, T>> next) where T : class
+        {
+            throw new NotImplementedByDesignException("Redis saga repository does not support queries");
+        }
+
+        public void Probe(ProbeContext context)
+        {
+            var scope = context.CreateScope("sagaRepository");
+            scope.Set(new
+            {
+                Persistence = "redis"
+            });
+        }
+
+        private static Task<bool> PreInsertSagaInstance<T>(IRedisTypedClient<TSaga> sagas, TSaga instance)
+        {
+            try
+            {
+                sagas.Store(instance);
+
+                if (_log.IsDebugEnabled)
+                    _log.DebugFormat("SAGA:{0}:{1} Insert {2}", TypeMetadataCache<TSaga>.ShortName, instance.CorrelationId,
+                        TypeMetadataCache<T>.ShortName);
+                return Task.FromResult(true);
+            }
+            catch (Exception ex)
+            {
+                if (_log.IsDebugEnabled)
+                    _log.DebugFormat("SAGA:{0}:{1} Dupe {2} - {3}", TypeMetadataCache<TSaga>.ShortName,
+                        instance.CorrelationId,
+                        TypeMetadataCache<T>.ShortName, ex.Message);
+
+                return Task.FromResult(false);
+            }
+        }
+
+        /// <summary>
+        ///     Once the message pipe has processed the saga instance, add it to the saga repository
+        /// </summary>
+        /// <typeparam name="TMessage"></typeparam>
+        private class MissingPipe<TMessage> :
+            IPipe<SagaConsumeContext<TSaga, TMessage>>
+            where TMessage : class
+        {
+            private readonly IPipe<SagaConsumeContext<TSaga, TMessage>> _next;
+            private readonly IRedisTypedClient<TSaga> _sagas;
+
+            public MissingPipe(IRedisTypedClient<TSaga> sagas, IPipe<SagaConsumeContext<TSaga, TMessage>> next)
+            {
+                _sagas = sagas;
+                _next = next;
+            }
+
+            void IProbeSite.Probe(ProbeContext context)
+            {
+                _next.Probe(context);
+            }
+
+            public async Task Send(SagaConsumeContext<TSaga, TMessage> context)
+            {
+                if (_log.IsDebugEnabled)
+                {
+                    _log.DebugFormat("SAGA:{0}:{1} Added {2}", TypeMetadataCache<TSaga>.ShortName,
+                        context.Saga.CorrelationId,
+                        TypeMetadataCache<TMessage>.ShortName);
+                }
+
+                SagaConsumeContext<TSaga, TMessage> proxy = new RedisSagaConsumeContext<TSaga, TMessage>(_sagas,
+                    context, context.Saga);
+
+                await _next.Send(proxy).ConfigureAwait(false);
+
+                if (!proxy.IsCompleted)
+                    _sagas.Store(context.Saga);
+            }
+        }
+    }
+
+}
